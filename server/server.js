1722238a6d40242d65775b6834d5b137b9f0f9f5
IsmaeilSaberi <ismaeilsaberi@gmail.com>
2023-03-28 06:39:02 -0700

    I create server part of fileshop project, and I added necessary packages and created server.js file and did some configs for connect to db and etc

---
+++ server/server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+
+// importing installed packages
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv/config");
+
+// importing security packages
+const helmet = require("helmet");
+const xssCleaner = require("xss-clean");
+const mongoSanitize = require("express-mongo-sanitize");
+const hpp = require("hpp");
+
+// using middlewares
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
+app.use(cookieParser());
+app.use(cors());
+
+//using security middlewares
+app.use(helmet());
+app.use(xssCleaner());
+app.use(mongoSanitize());
+app.use(hpp());
+
+// using project routes
+app.get("/", (req, res) => {
+  res.status(200).json({ msg: "this is the server of the fileshop project!" });
+});
+
+// connecting to data base
+const PORT = process.env.PORT;
+const DB_URL = process.env.DB_URL;
+
+mongoose
+  .connect(DB_URL)
+  .then((d) => {
+    app.listen(PORT);
+  })
+  .catch((err) => console.log(err));
