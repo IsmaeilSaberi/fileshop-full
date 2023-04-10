9b3a969e0bae7642dd4f4d3fa0efdb5a1cb78f54
IsmaeilSaberi <ismaeilsaberi@gmail.com>
2023-04-09 21:53:00 -0700

    blog page completed successfully with it's components!

--- client/app/blog/page.jsx
+++ client/app/blog/page.jsx
@@ -1,5 +1,16 @@
-const BlogPage = () => {
-  return <div>this is blog</div>;
+import BlogPageComp from "../../components/blog-page";
+
+const BlogPage = async () => {
+  return (
+    <div className="container mx-auto">
+      <section className="flex flex-col gap-8">
+        <h1 className="text-center text-xl text-indigo-600">
+          وبلاگ فروشگاه فایل اسماعیل صابری
+        </h1>
+        <BlogPageComp />
+      </section>
+    </div>
+  );
 };
 
 export default BlogPage;
---
+++ client/components/blog-page/index.jsx
@@ -0,0 +1,103 @@
+"use client";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import BlogBox from "../blogs/blogbox";
+import Image from "next/image";
+
+const BlogPageComp = () => {
+  const [posts, setPosts] = useState([-1]);
+  const [btnNumbers, setBtnNumbers] = useState([-1]);
+  const [filteredBtns, setFilteredBtns] = useState([-1]);
+  const [pageNumber, setPageNumber] = useState(1);
+  const paginate = 4;
+
+  const goToTop = () => {
+    window.scroll({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://fileshop-server.iran.liara.run/api/get-blog-page-posts?pn=${pageNumber}&pgn=${paginate}`
+      )
+      .then((d) => {
+        setPosts(d.data.GoalPosts);
+        setBtnNumbers([
+          ...Array(Math.ceil(d.data.AllPostsNumber / paginate)).keys(),
+        ]);
+        setAllPostsNumbers(d.data.AllPostsNumber);
+      })
+      .catch((err) => console.log(err));
+  }, [pageNumber]);
+
+  useEffect(() => {
+    if (btnNumbers[0] != -1 && btnNumbers.length > 0) {
+      const arr = [];
+      btnNumbers.map((n) => {
+        if (
+          n == 0 ||
+          (n < pageNumber + 1 && n > pageNumber - 3) ||
+          n == btnNumbers.length - 1
+        ) {
+          arr.push(n);
+        }
+      });
+      setFilteredBtns(arr);
+    } else if (btnNumbers.length == 0) {
+      setFilteredBtns([]);
+    }
+  }, [btnNumbers]);
+
+  return (
+    <div className=" flex flex-col gap-8">
+      <div className="flex flex-col gap-6">
+        {posts[0] == -1 ? (
+          <div className="flex justify-center items-center p-12">
+            <Image
+              alt="loading"
+              width={120}
+              height={120}
+              src={"/loading.svg"}
+            />
+          </div>
+        ) : posts.length < 1 ? (
+          <div className="flex justify-center items-center w-full p-8">
+            پستی موجود نیست!
+          </div>
+        ) : (
+          <div className="flex flex-wrap justify-between items-center gap-2">
+            {posts.map((post, i) => (
+              <BlogBox key={i} data={post} />
+            ))}
+          </div>
+        )}
+      </div>
+      <div className="flex justify-center items-center gap-2">
+        {filteredBtns[0] == -1 ? (
+          <div className="flex justify-center items-center p-12">
+            <Image alt="loading" width={40} height={40} src={"/loading.svg"} />
+          </div>
+        ) : (
+          filteredBtns.map((n, i) => (
+            <button
+              className="rounded-full w-8 h-8 bg-indigo-500 text-white flex justify-center items-center transition-all duration-300 hover:bg-orange-500"
+              onClick={() => {
+                setPageNumber(n + 1);
+                setPosts([-1]);
+                goToTop();
+              }}
+              key={i}
+            >
+              {n + 1}
+            </button>
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default BlogPageComp;
